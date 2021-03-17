fbeb15bc8c2f9f5eb482ef3e907973eb6f6a8062
adomakor412 <adomakor412@gmail.com>
2021-03-17 18:02:57 +0000

    working python script from cmd line, still some blanks

--- artifacts.py
+++ artifacts.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+'!/usr/bin/python3'
+
 import sys
-from netCDF4 import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
 #from mpl_toolkits.basemap import Basemap
@@ -27,11 +29,11 @@ from multiprocessing import Pool
 import warnings
 import logging
 
-logger = logging.getLogger()
+#logger = logging.getLogger()
 
-store = '../artifacts/'
-sharkfins_FD_NC = '/scratch/gops/amqpfind/adomako_data/l1b_imagery_sharkfin/'
-caterpillar_FD_NC = '/scratch/gops/amqpfind/adomako_data/l1b_imagery_caterpillar_track/'
+# store = '../artifacts/'
+# sharkfins_FD_NC = '/scratch/gops/amqpfind/adomako_data/l1b_imagery_sharkfin/'
+# caterpillar_FD_NC = '/scratch/gops/amqpfind/adomako_data/l1b_imagery_caterpillar_track/'
 
 def Rad2BT(rad, planck_fk1, planck_fk2, planck_bc1, planck_bc2):
     """Radiances to Brightness Temprature (using black body equation)"""
@@ -44,71 +46,76 @@ def Rad2BT(rad, planck_fk1, planck_fk2, planck_bc1, planck_bc2):
     T = (- planck_bc1+(planck_fk2 * (np.log(arg)**(-1))) )*(1/planck_bc2)
     return T
 
-def download(url,toPath, saveName):
-    cmd = [ 'wget ' + url +' -P ' + toPath +' -O '+ saveName]#if re.search('C07',url)
-    #print(cmd)
-    pid = Popen(cmd, shell=True)
-    pid.communicate()
-    return
+# def download(url,toPath, saveName):
+#     cmd = [ 'wget ' + url +' -P ' + toPath +' -O '+ saveName]#if re.search('C07',url)
+#     #print(cmd)
+#     pid = Popen(cmd, shell=True)
+#     pid.communicate()
+#     return
 
-Sat = [16,17]
-year = [2021]
-month = list(range(1,3))
-day = list(range(1,32))
-hour = list(range(1,24))
 
-search = list(itertools.product(Sat, year, month, day, hour))
 
-def execute(artifact,storage):
-    filelog = open('log_artifact.txt','w')
+# Sat = [16,17]
+# year = [2021]
+# month = list(range(1,3))
+# day = list(range(1,32))
+# hour = list(range(1,24))
+
+# search = list(itertools.product(Sat, year, month, day, hour))
+
+def main(argv):
+    file = argv[0]
+    
+    #filelog = open('log_artifact.txt','w')
     try:
-        netcdf = glob.glob(op.join(artifact,'*.nc'))
-        print(netcdf, file=filelog)
-        for file in netcdf:
-            '''
-            stackoverflow.com/questions/13714454/specifying-and-saving-a-figure-with-exact-size-in-pixels
-            '''
-            my_dpi = 192
-            resolution = 5424
-            GOES_R = xr.open_dataset(file)
-            GOES_image = GOES_R['Rad']
-            planck_fk1 = float(GOES_R['planck_fk1'].data)
-            planck_fk2 = float(GOES_R['planck_fk2'].data) 
-            planck_bc1 = float(GOES_R['planck_bc1'].data)                       
-            planck_bc2 = float(GOES_R['planck_bc2'].data)
-            Kelvin_GOES_image = Rad2BT(GOES_image, planck_fk1, planck_fk2, planck_bc1, planck_bc2)
-            plt.figure(figsize=(resolution/my_dpi, resolution/my_dpi), dpi=my_dpi)
-            fig1 = plt.imshow(Kelvin_GOES_image, interpolation='none', vmin=180, vmax=300, cmap='plasma')
-            plt.grid(None)
-            plt.axis('off')
-            naming = f'{os.path.basename(os.path.normpath(file[:-3]))}_UTC_5424x5424.png'           
-            savePath = op.join(store,storage)       
-            if not os.path.isfile(op.join(savePath,naming)):
-                fig1.figure.savefig( op.join(savePath,naming) )
-            #cbar = plt.colorbar()
-            #cbar.ax.set_ylabel('Kelvin')
-            naming = f'{os.path.basename(os.path.normpath(file[:-3]))}_UTC_5424x5424_BW.png'
-            fig1=plt.imshow(Kelvin_GOES_image, cmap ='Greys', vmin=180, vmax=300)    
-            if not os.path.isfile(op.join(savePath,naming)):
-                fig1.figure.savefig(op.join(savePath,naming))
-            plt.close('all')
-            GOES_R.close()
+        #netcdf = glob.glob(op.join(artifact,'*.nc'))
+        #print(netcdf, file=filelog)
+        
+        my_dpi = 192
+        resolution = 5424
+        GOES_R = xr.open_dataset(file)
+        GOES_image = GOES_R['Rad']
+        planck_fk1 = float(GOES_R['planck_fk1'].data)
+        planck_fk2 = float(GOES_R['planck_fk2'].data) 
+        planck_bc1 = float(GOES_R['planck_bc1'].data)                       
+        planck_bc2 = float(GOES_R['planck_bc2'].data)
+        Kelvin_GOES_image = Rad2BT(GOES_image, planck_fk1, planck_fk2, planck_bc1, planck_bc2)
+        plt.figure(figsize=(resolution/my_dpi, resolution/my_dpi), dpi=my_dpi)
+        fig1 = plt.imshow(Kelvin_GOES_image, interpolation='none', vmin=180, vmax=300, cmap='plasma')
+        plt.grid(None)
+        plt.axis('off')
+        naming = str(os.path.basename(os.path.normpath(str(file)[:-3])))+'_UTC_5424x5424.png'           
+        savePath = argv[1]#op.join(store,storage)       
+        if not os.path.isfile(op.join(savePath,naming)):
+            fig1.figure.savefig( op.join(savePath,naming) )
+        #cbar = plt.colorbar()
+        #cbar.ax.set_ylabel('Kelvin')
+        naming = str(os.path.basename(os.path.normpath(file[:-3])))+'_UTC_5424x5424_BW.png'
+        fig1=plt.imshow(Kelvin_GOES_image, cmap ='Greys', vmin=180, vmax=300)    
+        if not os.path.isfile(op.join(savePath,naming)):
+            fig1.figure.savefig(op.join(savePath,naming))
+        plt.close('all')
+        GOES_R.close()
 
     except ValueError as e:
         print('THERE IS AN ERROR')
-        print('\n', file = filelog)
-        print(file, file = filelog)
-        print(e, file = filelog)
-        print('\n', file = filelog)
+        print(file)
+#         print('\n', file = filelog)
+#         print(file, file = filelog)
+#         print(e, file = filelog)
+#         print('\n', file = filelog)
 
-    filelog.close()
+    #filelog.close()
     return
 
-artifactList = [sharkfins_FD_NC, caterpillar_FD_NC]
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
-for artifact in artifactList:
-    if artifact == sharkfins_FD_NC:
-        storage = 'sharkfin'
-    else:
-        storage = 'caterpillar'
-    execute(artifact,storage)
+# artifactList = [sharkfins_FD_NC, caterpillar_FD_NC]
+
+# for artifact in artifactList:
+#     if artifact == sharkfins_FD_NC:
+#         storage = 'sharkfin'
+#     else:
+#         storage = 'caterpillar'
+#     execute(artifact,storage)
